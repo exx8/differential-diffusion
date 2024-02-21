1d3a16223cf1f802507354cded91c4d8d35a0405
EtienneDosSantos <130935112+EtienneDosSantos@users.noreply.github.com>
2024-02-21 16:37:09 +0100

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+set -e
+cd "$(dirname "$0")"
+# Check for Python and exit if not found
+if ! [ -x "$(command -v python)" ]; then
+  echo 'Error: python is not installed.' >&2
+  exit 1
+fi
+# Create a virtual environment
+if [ ! -d "venv" ]; then
+  python -m venv venv
+fi
+
+# Upgrade pip before `pip install`
+python -m pip install --upgrade pip
+
+# Install the custom diffusers version from GitHub
+./venv/bin/pip3 install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu118
+
+# Install other requirements
+./venv/bin/pip install -r requirements.txt
+
+echo "Installation completed."
